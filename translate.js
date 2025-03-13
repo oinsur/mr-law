567af8773f81c44b515cdcea3b2799f6acb4ecab
Omar Insurriaga <oinsur@gmail.com>
2025-03-13 09:26:02 -0600

    added formsubmit integration

--- translate.js
+++ translate.js
@@ -25,6 +25,29 @@ const translations = {
     "legal-desc": "Nuestro bufete de abogados tiene como objetivo proporcionar una amplia gama de servicios legales para satisfacer sus necesidades. Nuestros abogados experimentados están dedicados a ofrecer resultados excepcionales y una atención personalizada a cada cliente.",
     "practice-areas": "Áreas de Práctica",
     "attorneys": "Abogados",
+    // Practice section
+    "practice-title": "Nuestras Áreas de Práctica",
+    "practice-crimianal": "Defensa Criminal",
+    "practice-criminal-desc": "En el ámbito del derecho penal, nos dedicamos a defender los derechos de las personas que enfrentan cargos criminales. Nuestro enfoque combina estrategias de defensa agresivas con un apoyo compasivo al cliente. Investigamos meticulosamente las circunstancias de cada caso, asegurando que se exploren todas las opciones legales. Ya sea negociando acuerdos de culpabilidad o defendiendo en la corte, nuestro objetivo es lograr el mejor resultado posible para nuestros clientes...",
+    "learn-more": "Más información",
+    "practice-civil": "Derechos Civiles",
+    "practice-civil-desc": "Nuestra práctica de Derechos Civiles se dedica a proteger y defender los derechos fundamentales y las libertades garantizadas por la ley. Nos enfocamos en casos de discriminación, mala conducta policial, arrestos injustificados y violaciones de derechos constitucionales.<br><br>Creemos que todas las personas merecen un trato justo y protección igualitaria...",
+    "practice-disputes": "Disputas Civiles",   
+    "practice-disputes-desc": "Nuestra práctica de Derechos Civiles se dedica a proteger y defender los derechos fundamentales y las libertades garantizadas por la ley. Nos enfocamos en casos de discriminación, mala conducta policial, arrestos injustificados y violaciones de derechos constitucionales.<br><br>Creemos que todas las personas merecen un trato justo y protección igualitaria...",   
+    "practice-guardianship": "Tutela", 
+    "practice-guardianship-desc": "Nuestra práctica de Ley de Tutela ofrece servicios legales integrales para personas que buscan establecer la tutela de menores o adultos que no pueden cuidarse por sí mismos. Entendemos que navegar por asuntos de tutela puede ser emocionalmente complejo y legalmente desafiante, y estamos aquí para guiarlo en cada paso del proceso.<br><br>Ayudamos a nuestros clientes a comprender los aspectos legales...",   
+    "practice-divorce": "Divorcio y Custodia",  
+    "practice-divorce-desc": "Nuestra práctica de Ley de Divorcio está dedicada a guiar a individuos y familias a través de las complejidades del divorcio con compasión y experiencia. Entendemos que este es un momento difícil, y nuestro objetivo es brindar el apoyo y la representación legal necesarios para lograr una resolución justa y equitativa.<br><br>Manejamos todos los aspectos del proceso de divorcio, incluyendo la división de bienes, la manutención conyugal y la custodia de los hijos...",
+    "practice-link": "Conozca más sobre las áreas de práctica en las que nuestro bufete de abogados se destaca.",
+    // consultation section
+    "consultation-title": "Agenda una consulta hoy",
+    "consultation-desc": "Descubre cómo nuestros abogados experimentados pueden ayudarte con tus necesidades legales",
+    "schedule": "Agendar",
+    //===========================End of home page translations===========================
+    // Practice page
+    
+   
+    
     // footer links
     "links-title": "Enlaces Rápidos",
     "home": "Inicio",
@@ -35,7 +58,8 @@ const translations = {
     "hours-saturday": "Sábado: ",
     "hours-sunday": "Domingo: ",
     "hours-appointment": "Solo con Cita",
-    "hours-closed": "Cerrado"
-    //===========================End of home page translations===========================
-    // Practice page 
+    "hours-closed": "Cerrado",
+    // copyright
+    "copyright-rights": "2025 Markowski Ruvalcava Law Firm, LLC. Todos los derechos reservados.",
+    "copyright-design": "Diseño por: <span>Omar Insurriaga</span>"
 };
